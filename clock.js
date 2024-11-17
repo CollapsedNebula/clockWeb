dd42cb24567c5c808bf544c50e12b58631cd49fc
CollapsedNebula <3957yi@naver.com>
2024-11-17 21:14:45 +0900

    modify design

--- clock.js
+++ clock.js
@@ -17,10 +17,40 @@ function updateClock() {
 
     const secondDegree = seconds * 6;
     const minuteDegree = minutes * 6;
-    const hourDegree = hours * 15
+    const hourDegree = hours * 30;
     document.getElementById('secondSlice').style.transform = `rotate(${secondDegree}deg)`;
     document.getElementById('minuteSlice').style.transform = `rotate(${minuteDegree}deg)`;
     document.getElementById('hourSlice').style.transform = `rotate(${hourDegree}deg)`;
+}
+
+var darkModeValue = 0
+function darkMode() {
+    if (darkModeValue === 0) { //Light Mode
+        document.getElementById('darkMode').style.transform = 'translate(10%, -50%)';
+        document.getElementById('sun').style.opacity = 0;
+        document.getElementById('moon').style.opacity = 1;
+        document.getElementById('darkMode').style.backgroundColor = '#ccc';
+        document.getElementById('darkModeContainer').style.backgroundColor = '#efefef';
+        document.getElementById('lore').style.color = '#fff';
+        document.body.style.backgroundColor = '#000000';
+        document.getElementById('secondChart').style.borderColor = '#efefef';
+        document.getElementById('minuteChart').style.borderColor = '#efefef';
+        document.getElementById('hourChart').style.borderColor = '#efefef';
+        darkModeValue = 1;
+    }
+    else if (darkModeValue === 1) { //Dark Mode
+        document.getElementById('darkMode').style.transform = 'translate(-110%, -50%)';
+        document.getElementById('sun').style.opacity = 1;
+        document.getElementById('moon').style.opacity = 0;
+        document.getElementById('darkMode').style.backgroundColor = '#efefef';
+        document.getElementById('darkModeContainer').style.backgroundColor = '#ccc';
+        document.getElementById('lore').style.color = '#000';
+        document.body.style.backgroundColor = '#fff';
+        document.getElementById('secondChart').style.borderColor = '#ccc';
+        document.getElementById('minuteChart').style.borderColor = '#ccc';
+        document.getElementById('hourChart').style.borderColor = '#ccc';
+        darkModeValue = 0;
+    }
 }
 
 setInterval(updateClock, 1000);
